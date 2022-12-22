624bc5983e2c6958e135485c3a1a48075add97c3
Ji-yong219 <wldydslapjyy@naver.com>
2022-12-23 00:10:11 +0900

    Feat: indexPage
    indexPage 함수 추가

--- newyearletter/src/main/java/com/newyearletter/newyearletter/NewyearletterApplication.java
+++ newyearletter/src/main/java/com/newyearletter/newyearletter/NewyearletterApplication.java
@@ -2,12 +2,20 @@ package com.newyearletter.newyearletter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class NewyearletterApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NewyearletterApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	String indexPage() {
+		return "2023 New Year's Resoulution & Letter Service";
 	}
 
 }
