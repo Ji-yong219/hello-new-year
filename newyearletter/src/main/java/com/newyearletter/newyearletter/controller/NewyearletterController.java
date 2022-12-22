b49a03fddf9a4f4cdbaf572bdde5d166e6af602d
Ji-yong219 <wldydslapjyy@naver.com>
2022-12-23 01:53:17 +0900

    Feat: wireframe 0.1, 0.2
    와이어프레임 파일 추가

--- newyearletter/src/main/java/com/newyearletter/newyearletter/controller/NewyearletterController.java
+++ newyearletter/src/main/java/com/newyearletter/newyearletter/controller/NewyearletterController.java
@@ -1,19 +1,27 @@
-package com.newyearletter.newyearletter;
+package com.newyearletter.newyearletter.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/*
+ * 
+ * 파일명 : NewyearletterController.java
+ * 설명 : new year letter 메인 화면 컨트롤러
+ * 날짜 : 2022. 12. 23.
+ * 작성자 : 박지용
+ * 변경 이력 : 
+ */
 @SpringBootApplication
 @RestController
-public class NewyearletterApplication {
+public class NewyearletterController {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NewyearletterApplication.class, args);
+		SpringApplication.run(NewyearletterController.class, args);
 	}
 
-	@RequestMapping("/")
+	@RequestMapping("/") //, method={RequestMethod.POST, RequestMethod.PUT})
 	String indexPage() {
 		return "2023 New Year's Resoulution & Letter Service";
 	}
