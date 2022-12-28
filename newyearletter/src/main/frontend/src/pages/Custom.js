6ca3cdcf4bc54796803c396c470b053d72a7efb4
Ji-yong219 <comjiyong@kakao.com>
2022-12-28 23:31:11 +0900

    refactor: custom page

--- newyearletter/src/main/frontend/src/pages/Custom.js
+++ newyearletter/src/main/frontend/src/pages/Custom.js
@@ -7,6 +7,7 @@ import CustomRabbit from '../components/Rabbit'
 import { WISH_INIT_STATE } from '../utils/constant'
 import { SmallText } from './InviteLetter'
 import { Contour, SubTitle, Wrapper } from './Main'
+import CustomContainer from '../components/CustomContainer'
 
 function Custom() {
   const [wish, setWish] = React.useState(WISH_INIT_STATE)
@@ -23,7 +24,13 @@ function Custom() {
 
         <Wrapper gap={2}>
           <SmallText>올해, 나만의 토끼를 꾸며보세요!</SmallText>
-          <CustomRabbit />
+          <CustomContainer
+            money={info.money}
+            debug={false}
+            color={info.custom.split(';')[0]}
+            accessory={info.custom.split(';')[1]}
+            isCustom={false}
+          />
         </Wrapper>
       </Wrapper>
       <ButtonItem> 커스텀</ButtonItem>
