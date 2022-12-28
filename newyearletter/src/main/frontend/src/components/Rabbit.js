97361c0255b70968d9849141c7ff2f0bc7bad08b
Ji-yong219 <comjiyong@kakao.com>
2022-12-29 04:26:55 +0900

    style: font color

--- newyearletter/src/main/frontend/src/components/Rabbit.js
+++ newyearletter/src/main/frontend/src/components/Rabbit.js
@@ -43,128 +43,128 @@ const customizePickerStyle = {
   height: "fit-content"
 }
 
-  function CustomRabbit({color, accessory, isCustom = false}) {
-    // Customizing
-    $(".RabbitContainer").css({
-      position: "relative",
-      width: "360px",
-      height: "420px",
-      textAlign: "center", 
-    })
-    $($(".rabbit")[color-1]).css({
+function CustomRabbit({color, accessory, isCustom = false}) {
+  // Customizing
+  $(".RabbitContainer").css({
+    position: "relative",
+    width: "360px",
+    height: "420px",
+    textAlign: "center", 
+  })
+  $($(".rabbit")[color-1]).css({
+    display: "block",
+  })
+  
+  $(".accessory").css({
+    position: "absolute",
+    flex: "1"
+  })
+  if({accessory} != 0){
+    $($(".accessory")[accessory-1]).css({
       display: "block",
     })
-    
-    $(".accessory").css({
-      position: "absolute",
-      flex: "1"
+  }
+
+  $("#Carrot").css({
+    width: "26%",
+    left: "49%",
+    top: "39%",
+    zIndex: "5002"
+  })
+  $("#Cushion").css({
+    width: "100%",
+    left: "50%",
+    transform: "translateX(-50%)",
+    top: "60%",
+    zIndex: "4999"
+  })
+  $("#Flower").css({
+    width: "33.5%",
+    left: "26%",
+    top: "9.5%"
+  })
+  $("#Hanbok").css({
+    width: "72.5%",
+    left: "13%",
+    top: "40%",
+    zIndex: "5001"
+  })
+  $("#Hanbok2").css({
+    width: "70.9%",
+    left: "14%",
+    top: "40%",
+    zIndex: "5001"
+  })
+  $("#Pocket").css({
+    width: "23.5%",
+    left: "50%",
+    top: "40.5%",
+    zIndex: "5002"
+  })
+  $("#Ribbon").css({
+    width: "30%",
+    left: "36%",
+    top: "14%"
+  })
+  $("#Sunglasses").css({
+    width: "33%",
+    left: "39%",
+    top: "29%"
+  })
+
+
+
+  $(".customizePicker tr").css({
+    borderBottom: "1px solid rgba(202, 191, 176, 0.5)",
+  })
+  $(".customizePicker th,.customizePicker td").css({
+    height: "40px",
+    color: "rgba(202, 191, 176, 0.5)",
+    textAlign: "left",
+    fontFamily: "nanumRound",
+    fontSize: "18px"
+  })
+  $(".customizePicker button,.customizePicker .accessoryIcon").css({
+    width: "25px",
+    height: "25px",
+    borderRadius: "50%",
+    border: "none",
+    cursor: "pointer",
+    marginLeft: "10px",
+  })
+  $(".customizePicker .accessoryIcon").css({
+    borderRadius: "0",
+    border: "none",
+    objectFit: "scale-down",
+  })
+  
+  $(".customizePicker .colorList button").each((i, el) => {
+    $(el).css({
+      background: rabbitList[i][0],
+      border: "0.5px solid gray",
     })
-    if({accessory} != 0){
-      $($(".accessory")[accessory-1]).css({
-        display: "block",
-      })
+  })
+  
+  $(document)
+  .on("click", ".colorList button", (e) => {
+    const index = $(e.target).parent().index() - 1
+    const rabbits = document.getElementsByClassName("rabbit")
+
+    $(rabbits).css("display", "none")
+    rabbits[index].style.display = "block"
+    rabbits[index].style.width = "80%"
+  })
+  .on("click", ".accessoryList img", (e) => {
+    const index = $(e.target).parent().index() - 1
+    const accessorys = document.getElementsByClassName("accessory")
+
+    $(accessorys).css("display", "none")
+    if (accessorys[index].style.display === "none") {
+      accessorys[index].style.display = "block";
+    } else {
+      accessorys[index].style.display = "none";
     }
-
-    $("#Carrot").css({
-      width: "26%",
-      left: "49%",
-      top: "39%",
-      zIndex: "5002"
-    })
-    $("#Cushion").css({
-      width: "100%",
-      left: "50%",
-      transform: "translateX(-50%)",
-      top: "60%",
-      zIndex: "4999"
-    })
-    $("#Flower").css({
-      width: "33.5%",
-      left: "26%",
-      top: "9.5%"
-    })
-    $("#Hanbok").css({
-      width: "72.5%",
-      left: "13%",
-      top: "40%",
-      zIndex: "5001"
-    })
-    $("#Hanbok2").css({
-      width: "70.9%",
-      left: "14%",
-      top: "40%",
-      zIndex: "5001"
-    })
-    $("#Pocket").css({
-      width: "23.5%",
-      left: "50%",
-      top: "40.5%",
-      zIndex: "5002"
-    })
-    $("#Ribbon").css({
-      width: "30%",
-      left: "36%",
-      top: "14%"
-    })
-    $("#Sunglasses").css({
-      width: "33%",
-      left: "39%",
-      top: "29%"
-    })
-
-
-
-    $(".customizePicker tr").css({
-      borderBottom: "1px solid rgba(202, 191, 176, 0.5)",
-    })
-    $(".customizePicker th,.customizePicker td").css({
-      height: "40px",
-      color: "rgba(202, 191, 176, 0.5)",
-      textAlign: "left",
-      fontFamily: "nanumRound",
-      fontSize: "18px"
-    })
-    $(".customizePicker button,.customizePicker .accessoryIcon").css({
-      width: "25px",
-      height: "25px",
-      borderRadius: "50%",
-      border: "none",
-      cursor: "pointer",
-      marginLeft: "10px",
-    })
-    $(".customizePicker .accessoryIcon").css({
-      borderRadius: "0",
-      border: "none",
-      objectFit: "scale-down",
-    })
-    
-    $(".customizePicker .colorList button").each((i, el) => {
-      $(el).css({
-        background: rabbitList[i][0],
-        border: "0.5px solid gray",
-      })
-    })
-    
-    $(document)
-    .on("click", ".colorList button", (e) => {
-      const index = $(e.target).parent().index() - 1
-      const rabbits = document.getElementsByClassName("rabbit")
-
-      $(rabbits).css("display", "none")
-      rabbits[index].style.display = "block"
-      rabbits[index].style.width = "80%"
-    })
-    .on("click", ".accessoryList img", (e) => {
-      const index = $(e.target).parent().index() - 1
-      const accessorys = document.getElementsByClassName("accessory")
-
-      $(accessorys).css("display", "none")
-      if (accessorys[index].style.display === "none") {
-        accessorys[index].style.display = "block";
-      } else {
-        accessorys[index].style.display = "none";
-      }
-    });
+  });
 
   return(
     <>
