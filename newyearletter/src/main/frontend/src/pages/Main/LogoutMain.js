5a3e1034ed8b825163d0212149a808b900c720ad
Heebeom Yang <takeny1998@gmail.com>
2022-12-29 20:25:06 +0900

    feat: add rabbit to main

--- newyearletter/src/main/frontend/src/pages/Main/LogoutMain.js
+++ newyearletter/src/main/frontend/src/pages/Main/LogoutMain.js
@@ -1,8 +1,10 @@
 import { useNavigate } from 'react-router'
+import styled from 'styled-components'
 import ButtonItem from '../../components/ButtonItem'
 import Container from '../../components/Container'
 import Logo from '../../components/Logo'
 import { SubTitle, Wrapper } from '../Main'
+import Rabbit from '../../assets/images/main.png'
 
 function LogoutMain() {
   const navigate = useNavigate()
@@ -12,18 +14,25 @@ function LogoutMain() {
         <Logo />
         <SubTitle>2023년, 흑토끼의 묘한 편지함을 찾아오세요</SubTitle>
       </Wrapper>
-      <Wrapper>
-        <ButtonItem onClick={() => navigate('/login')}>로그인</ButtonItem>
-
-        <ButtonItem
-          background="--pink-100"
-          onClick={() => navigate('/sign-up')}
-        >
-          회원가입
-        </ButtonItem>
+      <Wrapper gap={4}>
+        <RabbitContainer src={Rabbit} />
+        <Wrapper>
+          <ButtonItem onClick={() => navigate('/login')}>로그인</ButtonItem>
+          <ButtonItem
+            background="--pink-100"
+            onClick={() => navigate('/sign-up')}
+          >
+            회원가입
+          </ButtonItem>
+        </Wrapper>
       </Wrapper>
     </Container>
   )
 }
+
+const RabbitContainer = styled.img`
+  width: 70%;
+  object-fit: cover;
+`
 
 export default LogoutMain
