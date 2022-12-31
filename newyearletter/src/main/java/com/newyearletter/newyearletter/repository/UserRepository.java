51505e5dd22c56a0ecabda819a7e9badd514bc5c
sujin-park0607 <goweg0607@gmail.com>
2022-12-31 19:39:45 +0900

    refactor: getList 수정

--- newyearletter/src/main/java/com/newyearletter/newyearletter/repository/UserRepository.java
+++ newyearletter/src/main/java/com/newyearletter/newyearletter/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.newyearletter.newyearletter.repository;
 import com.newyearletter.newyearletter.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.awt.print.Pageable;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Integer> {
